e2af5f1417e1a1a178462443b6ba2dc84ee24b32
i888844 <i888844@yandex.ru>
2023-12-14 10:14:09 +0300

    2nd commit

--- Lab. №6.cpp
+++ Lab. №6.cpp
@@ -3,7 +3,7 @@
 
 #define WITH_MANUAL_MODE true
 #define MIN -10
-#define MAX -1
+#define MAX 10
 
 using namespace std;
 
@@ -49,17 +49,17 @@ public:
     }
     ~VECTOR()
     {
-	    if (lenght > 0)
-	    {
+        if (lenght > 0)
+        {
             delete[]array;
-	    }
+        }
     }
     void set_number_manual(int settable_lenght, int index)
     {
-	    if (array == nullptr)
-	    {
+        if (array == nullptr)
+        {
             delete[]array;
-	    }
+        }
         lenght = settable_lenght;
         array = new T[lenght];
         for (int i = 0; i < lenght; i++)
@@ -83,10 +83,10 @@ public:
     }
     void set_number_coefficient(int settable_lenght, int index, double first_coefficient, double second_coefficient)
     {
-	    if (array == nullptr)
-	    {
+        if (array == nullptr)
+        {
             delete[]array;
-	    }
+        }
         lenght = settable_lenght;
         array = new T[lenght];
         for (int i = 0; i < lenght; i++)
@@ -111,10 +111,10 @@ public:
     int get_lenght() const { return lenght; }
     const T operator [] (int index)const
     {
-	    if ((index >= 0) && (index < lenght))
-	    {
+        if ((index >= 0) && (index < lenght))
+        {
             return array[index];
-	    }
+        }
         else
         {
             cout << "[Ошибка]: Неверно указан индекс элемента." << endl;
@@ -191,15 +191,15 @@ public:
     }
     MATRIX(int settable_strings, int settable_columns)
     {
-	    if (settable_strings > 0 && settable_columns > 0)
-	    {
+        if (settable_strings > 0 && settable_columns > 0)
+        {
             strings = settable_strings;
             array = new VECTOR<T>[strings];
             for (int i = 0; i < strings; i++)
             {
                 array[i].set_number_random(settable_columns);
             }
-	    }
+        }
         else
         {
             strings = 0;
@@ -234,13 +234,13 @@ public:
     }
     ~MATRIX()
     {
-	    if (strings > 0)
-	    {
+        if (strings > 0)
+        {
             delete[]array;
-	    }
+        }
     }
     int get_strings() const { return strings; }
-	void output()
+    void output()
     {
         if (strings > 0)
         {
@@ -260,7 +260,7 @@ public:
         T parameter = 0.0;
         T avg = 0.0;
         int i = 0;
-    	int j = 0;
+        int j = 0;
         for (i = 0; i < strings; i++)
         {
             for (j = 0; j < array[i].get_lenght(); j++)
@@ -289,25 +289,25 @@ public:
         int j = 0;
         for (i = 0; i < strings; i++)
         {
-	        for (j = 0; j < array[i].get_lenght(); j++)
-	        {
+            for (j = 0; j < array[i].get_lenght(); j++)
+            {
                 avg_global += array[i][j];
-	        }
+            }
         }
         avg_global /= strings * array[i].get_lenght();
         for (i = 0; i < strings; i++)
         {
-	        for (j = 0; j < array[i].get_lenght(); j++)
-	        {
+            for (j = 0; j < array[i].get_lenght(); j++)
+            {
                 avg_string = array[i][j];
-	        }
+            }
             avg_string /= array[i].get_lenght();
             if (avg_string > avg_global)
             {
-	            for (j = 0; j < array[i].get_lenght(); j++)
-	            {
+                for (j = 0; j < array[i].get_lenght(); j++)
+                {
                     array[i][j] -= avg_global;
-	            }
+                }
             }
             avg_string = 0.0;
         }
@@ -342,10 +342,10 @@ public:
         int i = string;
         for (int j = 0; j < array[i].get_lenght(); j++)
         {
-	        if (array[i][j] != 0)
-	        {
+            if (array[i][j] != 0)
+            {
                 composition *= array[i][j];
-	        }
+            }
         }
         return composition;
     }
@@ -358,7 +358,7 @@ public:
 //    int max_composition_string_index = 0;
 //    int lenght = src_object.get_strings();
 //    int i = 0;
-//	for (i = 0; i < lenght; i++)
+//    for (i = 0; i < lenght; i++)
 //    {
 //        composition = src_object.composition(i);
 //        if (composition > max_composition)
@@ -415,6 +415,35 @@ template <typename T> VECTOR<T> generate_vector(const MATRIX<T>& src_object)
     return src_object[max_composition_string_index];
 }
 
+template <typename T> ostream& operator << (ostream& out, VECTOR<T>&src_object)
+{
+    out << endl;
+    out << "Вектор:" << endl;
+    for(int i = 0; i < src_object.get_lenght(); i++)
+    {
+        out << src_object[i] << "   ";
+    }
+    out << endl;
+    return out;
+}
+
+template <typename T> ostream& operator << (ostream& out, MATRIX<T>& src_object)
+{
+    out << endl;
+    out << "Матрица:" << endl;
+    int j = 0;
+    for (int i = 0; i < src_object.get_strings(); i++)
+    {
+        for (j = 0; j < src_object[i].get_lenght(); j++)
+        {
+            out << src_object[i][j] << "   ";
+        }
+        out << endl;
+    }
+    out << endl;
+    return out;
+}
+
 int main()
 {
     SetConsoleCP(1251);
@@ -422,11 +451,11 @@ int main()
     srand(time(nullptr));
     MATRIX<double> A(6, 8, 17.2, -1.1);
     double A_parameter = A.parameter();
-    cout << "Матрица A:" << endl;
+    cout << "Матрица <double>A:" << endl;
     A.output();
     cout << "Параметр для A: " << A_parameter << endl;
     cout << endl;
-    if (A_parameter > 0)
+    if (A_parameter < 0)
     {
         A.processing();
         cout << "Преобразованная матрица A:" << endl;
@@ -439,7 +468,7 @@ int main()
     cout << endl;
     MATRIX<double> B(5, 7, 5.9, 2.4);
     double B_parameter = B.parameter();
-    cout << "Матрица B:" << endl;
+    cout << "Матрица <double>B:" << endl;
     B.output();
     cout << "Параметр для B: " << B_parameter << endl;
     if (B_parameter < 0)
@@ -447,24 +476,86 @@ int main()
         B.processing();
         cout << "Преобразованная матрица B:" << endl;
         B.output();
-        VECTOR<double> VB = generate_vector(A);
+        VECTOR<double> VB = generate_vector(B);
         cout << "Сформированный вектор:" << endl;
         VB.output();
     }
     cout << endl;
     MATRIX<double> C(4, 6, 12.3, -3.9);
     double C_parameter = C.parameter();
-    cout << "Матрица C:" << endl;
-	C.output();
+    cout << "Матрица <double>C:" << endl;
+    C.output();
     cout << "Параметр для C: " << C_parameter << endl;
     if (C_parameter < 0)
     {
         C.processing();
         cout << "Преобразованная матрица C:" << endl;
         C.output();
-        VECTOR<double> VC = generate_vector(A);
+        VECTOR<double> VC = generate_vector(C);
         cout << "Сформированный вектор:" << endl;
         VC.output();
     }
+    cout << endl;
+    MATRIX<char> D(5, 7, 1.5, 22.1);
+    char D_parameter = D.parameter();
+    cout << "Матрица <char>D:" << endl;
+    D.output();
+    cout << "Параметр для D: " << D_parameter << endl;
+    if (D_parameter < 0)
+    {
+        D.processing();
+        cout << "Преобразованная матрица D:" << endl;
+        D.output();
+        VECTOR<char> VD = generate_vector(D);
+        cout << "Сформированный вектор:" << endl;
+        VD.output();
+    }
+    cout << endl;
+    MATRIX<int> E(2, 3, -15.3, -13.9);
+    int E_parameter = E.parameter();
+    cout << "Матрица <int>E:" << endl;
+    E.output();
+    cout << "Параметр для E: " << E_parameter << endl;
+    if (D_parameter < 0)
+    {
+        E.processing();
+        cout << "Преобразованная матрица E:" << endl;
+        E.output();
+        VECTOR<int> VE = generate_vector(E);
+        cout << "Сформированный вектор:" << endl;
+        VE.output();
+    }
+    cout << endl;
+    MATRIX<long> F(4, 5, 2.3, 0.9);
+    long F_parameter = F.parameter();
+    cout << "Матрица <long>F:" << endl;
+    F.output();
+    cout << "Параметр для F: " << F_parameter << endl;
+    if (F_parameter < 0)
+    {
+        F.processing();
+        cout << "Преобразованная матрица F:" << endl;
+        F.output();
+        VECTOR<long> VF = generate_vector(F);
+        cout << "Сформированный вектор:" << endl;
+        VF.output();
+    }
+    cout << endl;
+    MATRIX<long double> G(3, 6, -0.3, 5.2);
+    long double G_parameter = G.parameter();
+    cout << "Матрица <long double>G:" << endl;
+    G.output();
+    cout << G;
+    cout << "Параметр для G: " << F_parameter << endl;
+    if (G_parameter > 0)
+    {
+        G.processing();
+        cout << "Преобразованная матрица G:" << endl;
+        cout << G;
+        VECTOR<long double> VG = generate_vector(G);
+        cout << "Сформированный вектор:" << endl;
+        cout << VG;
+    }
+    
     return 0;
 }
